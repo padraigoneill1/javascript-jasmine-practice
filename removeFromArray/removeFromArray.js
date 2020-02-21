f40a02971cb92f21cf3cfbe1c1ee1bb7cccbd2a6
Padraig O Neill <contact.padraig.o.neill@gmail.com>
2020-02-21 23:58:54 +0000

    Finished remove from array exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    for ( arg = 0; arg < arguments.length; arg++) {
+        for (i = 0; i<array.length; i++) {
+            if (array[i] == arguments[arg]) array.splice(i,1)
+        }
+    }
+    return array
 }
 
 module.exports = removeFromArray
