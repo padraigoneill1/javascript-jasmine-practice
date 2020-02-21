17cacadc7a30bead672d51fef1e1a26b033d5f1f
Padraig O Neill <contact.padraig.o.neill@gmail.com>
2020-02-21 23:25:27 +0000

    Finished repeat string exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word,repeats) {
+    if (repeats < 0) return 'ERROR' 
+    result = ''
+    for (i=0 ; i < repeats;i++){
+        result +=word
+    }
+    return result
 }
+
+ repeatString;
 
 module.exports = repeatString
