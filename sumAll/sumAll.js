179f9fcb673b07a83f80723df2e0261f71a8376e
Padraig O Neill <contact.padraig.o.neill@gmail.com>
2020-02-22 00:32:17 +0000

    Finished sumall exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if (a < 0 || b < 0 || typeof a!= 'number' || typeof b!= 'number' ) return 'ERROR'
+    result = 0
+    i = 0
+    j = 0
+    if (a < b){
+        for (;a<=b;a++){
+            result +=a ;
+        }
+    }
+    else{
+        for (;a>=b;a--){
+            result +=a ;
+        }
+    }
+    return result
 }
 
 module.exports = sumAll
